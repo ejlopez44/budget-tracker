42ebc8778196356b23c18ee5f9bdf70326f6230f
Tito <ejlopez44@gmail.com>
2020-07-04 00:21:32 -0400

    offline functionality implemented

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,8 @@
 console.log("Service worker ready");
 
+// Resources I want to cache
 const FILES_TO_CACHE = [
+    '/',
     '/index.html',
     'index.js',
     '/manifest.webmanifest',
@@ -10,24 +12,27 @@ const FILES_TO_CACHE = [
     '/service-worker.js'
 ]
 
+// store all assets in static cache
 const CACHE_NAME = 'static-cache-v2';
+// this one is for data that comes back from server (need this to populate so that user can view previous transactions offline )
 const DATA_CACHE_NAME = 'data-cache-v1';
 
-// install
+// install service worker for the first time and immediately cache all files
 self.addEventListener("install", function (evt) {
+    // open cache and store all files
     evt.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
             console.log("Your files were pre-cached successfully!");
             return cache.addAll(FILES_TO_CACHE);
         })
     );
-
     self.skipWaiting();
 });
 
-// activate
+// base activate
 self.addEventListener("activate", function (evt) {
     evt.waitUntil(
+        // keys method returns a promise that resolves to an array of Cache keys.
         caches.keys().then(keyList => {
             return Promise.all(
                 keyList.map(key => {
@@ -43,8 +48,9 @@ self.addEventListener("activate", function (evt) {
     self.clients.claim();
 });
 
-// fetch
+// fetch data from api or cache dependent on connection
 self.addEventListener("fetch", function (evt) {
+    // first checks if making an api call
     if (evt.request.url.includes("/api/")) {
         evt.respondWith(
             caches.open(DATA_CACHE_NAME).then(cache => {
@@ -54,19 +60,18 @@ self.addEventListener("fetch", function (evt) {
                         if (response.status === 200) {
                             cache.put(evt.request.url, response.clone());
                         }
-
                         return response;
                     })
                     .catch(err => {
+                        console.log('NO NETWORK CONNECTION')
                         // Network request failed, try to get it from the cache.
                         return cache.match(evt.request);
                     });
             }).catch(err => console.log(err))
         );
-
         return;
     }
-
+    // if not an api call default respond with cache data
     evt.respondWith(
         caches.open(CACHE_NAME).then(cache => {
             return cache.match(evt.request).then(response => {
@@ -75,3 +80,25 @@ self.addEventListener("fetch", function (evt) {
         })
     );
 });
+
+// option for fetch request to use cache until return server, want to come back to this..
+// let networkDataReceived = false;
+// const networkUpdate = fetch('/api')
+//     .then(res => {
+//         return res.json()
+//     }).then(data => {
+//         networkDataReceived = true;
+//         updatePage(data)
+//     })
+
+// // need to edit route here most likely
+// caches.match('/api')
+//     .then(res => {
+//         return res.json()
+//     }).then(data => {
+//         if (!networkDataReceived) {
+//             updatePage(data)
+//         }
+//     }).catch(function(){
+//         return networkUpdate;
+//     })
